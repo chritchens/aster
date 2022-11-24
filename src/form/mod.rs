e10d993ef179c658b28bf7fb753a9acd7dea7a2e
chritchens <chritchens@gmail.com>
2022-11-24 08:12:59 +0100

    Rename BasicValue as SimpleValue

--- src/form/mod.rs
+++ src/form/mod.rs
@@ -1,6 +1,5 @@
 pub mod app_form;
 pub mod attrs_form;
-pub mod basic_value;
 pub mod case_form;
 pub mod export_form;
 #[allow(clippy::module_inception)]
@@ -11,6 +10,7 @@ pub mod let_form;
 pub mod module_form;
 pub mod prod_form;
 pub mod sig_form;
+pub mod simple_value;
 pub mod type_form;
 pub mod types_form;
 pub mod val_form;
---
+++ src/form/simple_value.rs
@@ -0,0 +1,136 @@
+use crate::error::{Error, SyntacticError};
+use crate::loc::Loc;
+use crate::result::Result;
+use crate::syntax::{is_type_keyword, is_value_keyword};
+use crate::token::{Token, TokenKind};
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum SimpleValue {
+    Ignore(Token),
+    Empty(Token),
+    Panic(Token),
+    ValueKeyword(Token),
+    TypeKeyword(Token),
+    Prim(Token),
+    ValueSymbol(Token),
+    TypeSymbol(Token),
+    ValuePathSymbol(Token),
+    TypePathSymbol(Token),
+    Unknown(Token),
+}
+
+impl Default for SimpleValue {
+    fn default() -> SimpleValue {
+        SimpleValue::Unknown(Token::new())
+    }
+}
+
+impl SimpleValue {
+    pub fn new() -> SimpleValue {
+        SimpleValue::default()
+    }
+
+    pub fn token(&self) -> Token {
+        match self {
+            SimpleValue::Ignore(token) => token.clone(),
+            SimpleValue::Empty(token) => token.clone(),
+            SimpleValue::Panic(token) => token.clone(),
+            SimpleValue::Prim(token) => token.clone(),
+            SimpleValue::ValueKeyword(token) => token.clone(),
+            SimpleValue::TypeKeyword(token) => token.clone(),
+            SimpleValue::ValueSymbol(token) => token.clone(),
+            SimpleValue::TypeSymbol(token) => token.clone(),
+            SimpleValue::ValuePathSymbol(token) => token.clone(),
+            SimpleValue::TypePathSymbol(token) => token.clone(),
+            SimpleValue::Unknown(token) => token.clone(),
+        }
+    }
+
+    pub fn file(&self) -> String {
+        match self {
+            SimpleValue::Ignore(token) => token.file(),
+            SimpleValue::Empty(token) => token.file(),
+            SimpleValue::Panic(token) => token.file(),
+            SimpleValue::ValueKeyword(token) => token.file(),
+            SimpleValue::TypeKeyword(token) => token.file(),
+            SimpleValue::Prim(token) => token.file(),
+            SimpleValue::ValueSymbol(token) => token.file(),
+            SimpleValue::TypeSymbol(token) => token.file(),
+            SimpleValue::ValuePathSymbol(token) => token.file(),
+            SimpleValue::TypePathSymbol(token) => token.file(),
+            SimpleValue::Unknown(token) => token.file(),
+        }
+    }
+
+    pub fn loc(&self) -> Option<Loc> {
+        match self {
+            SimpleValue::Ignore(token) => token.loc(),
+            SimpleValue::Empty(token) => token.loc(),
+            SimpleValue::Panic(token) => token.loc(),
+            SimpleValue::ValueKeyword(token) => token.loc(),
+            SimpleValue::TypeKeyword(token) => token.loc(),
+            SimpleValue::Prim(token) => token.loc(),
+            SimpleValue::ValueSymbol(token) => token.loc(),
+            SimpleValue::TypeSymbol(token) => token.loc(),
+            SimpleValue::ValuePathSymbol(token) => token.loc(),
+            SimpleValue::TypePathSymbol(token) => token.loc(),
+            SimpleValue::Unknown(token) => token.loc(),
+        }
+    }
+
+    pub fn from_token(token: &Token) -> Result<SimpleValue> {
+        let token = token.to_owned();
+
+        match token.kind {
+            TokenKind::Comment | TokenKind::DocComment => Err(Error::Syntactic(SyntacticError {
+                loc: token.loc(),
+                desc: "unexpected comment marker".into(),
+            })),
+            TokenKind::FormStart | TokenKind::FormEnd => Err(Error::Syntactic(SyntacticError {
+                loc: token.loc(),
+                desc: "unexpected form punctuation".into(),
+            })),
+            TokenKind::EmptyLiteral => Ok(SimpleValue::Empty(token)),
+            TokenKind::UIntLiteral
+            | TokenKind::IntLiteral
+            | TokenKind::FloatLiteral
+            | TokenKind::CharLiteral
+            | TokenKind::StringLiteral => Ok(SimpleValue::Prim(token)),
+            TokenKind::Keyword => match token.to_string().as_str() {
+                "_" => Ok(SimpleValue::Ignore(token)),
+                "panic" => Ok(SimpleValue::Panic(token)),
+                x if is_value_keyword(x) => Ok(SimpleValue::ValueKeyword(token)),
+                x if is_type_keyword(x) => Ok(SimpleValue::TypeKeyword(token)),
+                _ => unreachable!(),
+            },
+            TokenKind::ValueSymbol => Ok(SimpleValue::ValueSymbol(token)),
+            TokenKind::TypeSymbol => Ok(SimpleValue::TypeSymbol(token)),
+            TokenKind::ValuePathSymbol => Ok(SimpleValue::ValuePathSymbol(token)),
+            TokenKind::TypePathSymbol => Ok(SimpleValue::TypePathSymbol(token)),
+        }
+    }
+
+    #[allow(clippy::inherent_to_string_shadow_display)]
+    pub fn to_string(&self) -> String {
+        match self {
+            SimpleValue::Ignore(_) => "_".into(),
+            SimpleValue::Empty(_) => "()".into(),
+            SimpleValue::Panic(_) => "panic".into(),
+            SimpleValue::Prim(token) => token.to_string(),
+            SimpleValue::ValueKeyword(token) => token.to_string(),
+            SimpleValue::TypeKeyword(token) => token.to_string(),
+            SimpleValue::ValueSymbol(token) => token.to_string(),
+            SimpleValue::TypeSymbol(token) => token.to_string(),
+            SimpleValue::ValuePathSymbol(token) => token.to_string(),
+            SimpleValue::TypePathSymbol(token) => token.to_string(),
+            SimpleValue::Unknown(token) => token.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for SimpleValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
