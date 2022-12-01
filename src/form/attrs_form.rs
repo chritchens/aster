8f588dab349e09f2d5a094c48f464c211348cba7
chritchens <chritchens@gmail.com>
2022-12-01 22:41:32 +0100

    Use map in the attributes form

--- src/form/attrs_form.rs
+++ src/form/attrs_form.rs
@@ -1,6 +1,6 @@
 use crate::error::{Error, SyntacticError};
 use crate::form::form::{Form, FormTailElement};
-use crate::form::list_form::{ListForm, ListFormValue};
+use crate::form::map_form::MapForm;
 use crate::loc::Loc;
 use crate::result::Result;
 use crate::token::Tokens;
@@ -10,12 +10,13 @@ use std::fmt;
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum AttrsFormValue {
     Empty(SimpleValue),
-    ValueKeyword(SimpleValue),
-    TypeKeyword(SimpleValue),
+    Panic(SimpleValue),
+    Atomic(SimpleValue),
     ValueSymbol(SimpleValue),
     TypeSymbol(SimpleValue),
     ValuePathSymbol(SimpleValue),
     TypePathSymbol(SimpleValue),
+    Map(Box<MapForm>),
 }
 
 impl Default for AttrsFormValue {
@@ -28,24 +29,26 @@ impl AttrsFormValue {
     pub fn file(&self) -> String {
         match self {
             AttrsFormValue::Empty(empty) => empty.file(),
-            AttrsFormValue::ValueKeyword(keyword) => keyword.file(),
-            AttrsFormValue::TypeKeyword(keyword) => keyword.file(),
-            AttrsFormValue::ValueSymbol(symbol) => symbol.file(),
-            AttrsFormValue::TypeSymbol(symbol) => symbol.file(),
-            AttrsFormValue::ValuePathSymbol(symbol) => symbol.file(),
-            AttrsFormValue::TypePathSymbol(symbol) => symbol.file(),
+            AttrsFormValue::Panic(empty) => empty.file(),
+            AttrsFormValue::Atomic(value) => value.file(),
+            AttrsFormValue::ValueSymbol(value) => value.file(),
+            AttrsFormValue::TypeSymbol(value) => value.file(),
+            AttrsFormValue::ValuePathSymbol(value) => value.file(),
+            AttrsFormValue::TypePathSymbol(value) => value.file(),
+            AttrsFormValue::Map(form) => form.file(),
         }
     }
 
     pub fn loc(&self) -> Option<Loc> {
         match self {
             AttrsFormValue::Empty(empty) => empty.loc(),
-            AttrsFormValue::ValueKeyword(keyword) => keyword.loc(),
-            AttrsFormValue::TypeKeyword(keyword) => keyword.loc(),
-            AttrsFormValue::ValueSymbol(symbol) => symbol.loc(),
-            AttrsFormValue::TypeSymbol(symbol) => symbol.loc(),
-            AttrsFormValue::ValuePathSymbol(symbol) => symbol.loc(),
-            AttrsFormValue::TypePathSymbol(symbol) => symbol.loc(),
+            AttrsFormValue::Panic(value) => value.loc(),
+            AttrsFormValue::Atomic(value) => value.loc(),
+            AttrsFormValue::ValueSymbol(value) => value.loc(),
+            AttrsFormValue::TypeSymbol(value) => value.loc(),
+            AttrsFormValue::ValuePathSymbol(value) => value.loc(),
+            AttrsFormValue::TypePathSymbol(value) => value.loc(),
+            AttrsFormValue::Map(form) => form.loc(),
         }
     }
 
@@ -53,12 +56,13 @@ impl AttrsFormValue {
     pub fn to_string(&self) -> String {
         match self {
             AttrsFormValue::Empty(_) => "()".into(),
-            AttrsFormValue::ValueKeyword(keyword) => keyword.to_string(),
-            AttrsFormValue::TypeKeyword(keyword) => keyword.to_string(),
-            AttrsFormValue::ValueSymbol(symbol) => symbol.to_string(),
-            AttrsFormValue::TypeSymbol(symbol) => symbol.to_string(),
-            AttrsFormValue::ValuePathSymbol(symbol) => symbol.to_string(),
-            AttrsFormValue::TypePathSymbol(symbol) => symbol.to_string(),
+            AttrsFormValue::Panic(value) => value.to_string(),
+            AttrsFormValue::Atomic(value) => value.to_string(),
+            AttrsFormValue::ValueSymbol(value) => value.to_string(),
+            AttrsFormValue::TypeSymbol(value) => value.to_string(),
+            AttrsFormValue::ValuePathSymbol(value) => value.to_string(),
+            AttrsFormValue::TypePathSymbol(value) => value.to_string(),
+            AttrsFormValue::Map(form) => form.to_string(),
         }
     }
 }
@@ -104,18 +108,11 @@ impl AttrsForm {
     }
 
     pub fn values_to_string(&self) -> String {
-        match self.values.len() {
-            1 => self.values[0].to_string(),
-            x if x > 1 => format!(
-                "(list {})",
-                self.values
-                    .iter()
-                    .map(|p| p.to_string())
-                    .collect::<Vec<String>>()
-                    .join(" ")
-            ),
-            _ => "()".to_string(),
-        }
+        self.values
+            .iter()
+            .map(|p| p.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
     }
 
     pub fn all_parameters(&self) -> Vec<SimpleValue> {
@@ -172,11 +169,11 @@ impl AttrsForm {
                 SimpleValue::Empty(_) => {
                     attrs.values.push(AttrsFormValue::Empty(value));
                 }
-                SimpleValue::ValueKeyword(_) => {
-                    attrs.values.push(AttrsFormValue::ValueKeyword(value));
+                SimpleValue::Panic(_) => {
+                    attrs.values.push(AttrsFormValue::Panic(value));
                 }
-                SimpleValue::TypeKeyword(_) => {
-                    attrs.values.push(AttrsFormValue::TypeKeyword(value));
+                SimpleValue::Atomic(_) => {
+                    attrs.values.push(AttrsFormValue::Atomic(value));
                 }
                 SimpleValue::ValueSymbol(_) => {
                     attrs.values.push(AttrsFormValue::ValueSymbol(value));
@@ -198,39 +195,12 @@ impl AttrsForm {
                 }
             },
             FormTailElement::Form(form) => {
-                if let Ok(list) = ListForm::from_form(&form) {
-                    for value in list.values.iter() {
-                        match value.clone() {
-                            ListFormValue::TypeKeyword(keyword) => {
-                                attrs.values.push(AttrsFormValue::TypeKeyword(keyword));
-                            }
-                            ListFormValue::ValueKeyword(keyword) => {
-                                attrs.values.push(AttrsFormValue::ValueKeyword(keyword));
-                            }
-                            ListFormValue::TypeSymbol(symbol) => {
-                                attrs.values.push(AttrsFormValue::TypeSymbol(symbol));
-                            }
-                            ListFormValue::ValueSymbol(symbol) => {
-                                attrs.values.push(AttrsFormValue::ValueSymbol(symbol));
-                            }
-                            ListFormValue::TypePathSymbol(symbol) => {
-                                attrs.values.push(AttrsFormValue::TypePathSymbol(symbol));
-                            }
-                            ListFormValue::ValuePathSymbol(symbol) => {
-                                attrs.values.push(AttrsFormValue::ValuePathSymbol(symbol));
-                            }
-                            x => {
-                                return Err(Error::Syntactic(SyntacticError {
-                                    loc: x.loc(),
-                                    desc: "unexpected value".into(),
-                                }));
-                            }
-                        }
-                    }
+                if let Ok(map) = MapForm::from_form(&form) {
+                    attrs.values.push(AttrsFormValue::Map(Box::new(map)));
                 } else {
                     return Err(Error::Syntactic(SyntacticError {
                         loc: form.loc(),
-                        desc: "unexpected form".into(),
+                        desc: "expected a map form".into(),
                     }));
                 }
             }
@@ -317,7 +287,7 @@ mod tests {
         assert_eq!(form.values_to_string(), "moduleX.X".to_string());
         assert_eq!(form.to_string(), s.to_string());
 
-        s = "(attrs x (list union a moduleA.A Type))";
+        s = "(attrs x (map (prod union a) (prod moduleA.A Type)))";
 
         res = AttrsForm::from_str(s);
 
@@ -329,7 +299,7 @@ mod tests {
         assert!(form.is_value_attributes());
         assert_eq!(
             form.values_to_string(),
-            "(list union a moduleA.A Type)".to_string()
+            "(map (prod union a) (prod moduleA.A Type))".to_string()
         );
         assert_eq!(form.to_string(), s.to_string());
     }
