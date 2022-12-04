005694c5e6a1cf290a9658398a445284135cb8c2
chritchens <chritchens@gmail.com>
2022-12-04 22:11:21 +0100

    Add type structs and enums

--- src/typing/types.rs
+++ src/typing/types.rs
@@ -1,1 +1,53 @@
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct EnumType(Vec<Type>);
 
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct PairType {
+    pub first: Box<Type>,
+    pub second: Box<Type>,
+}
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct ListType(Vec<Type>);
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct ArrType(Vec<Type>);
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct VecType(Vec<Type>);
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct MapType(Vec<PairType>);
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct FunType {
+    pub args: Vec<Type>,
+    pub ret: Box<Type>,
+}
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub enum Type {
+    Builtin,
+    Empty,
+    Atomic,
+    UInt,
+    Int,
+    Float,
+    Size,
+    Pointer,
+    Ref,
+    Char,
+    String,
+    Mem,
+    Path,
+    IO,
+    Ctx,
+    Enum(Box<EnumType>),
+    Pair(Box<PairType>),
+    List(Box<ListType>),
+    Arr(Box<ArrType>),
+    Vec(Box<VecType>),
+    Map(Box<MapType>),
+    Fun(Box<FunType>),
+    Type,
+}
